cebdff98655d0c44772675cf993fbe7b5bcde67a
Matt! <mmneer@gmail.com>
2018-09-04 15:54:51 -0400

    Added Garduino directory

---
+++ Garduino/moisture/moisture.ino
@@ -0,0 +1,33 @@
+
+//one nail to V5 and the other to A0 w/ resistor
+int moistureSensor = A0;
+
+//storage variable
+int moisture = 0;
+
+//optimal moisture range
+int high = 150;
+int low = 100;
+
+//indicator LED
+int led = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(moistureSensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  moisture = analogRead(moistureSensor);
+  Serial.print("Moisture level: ");
+  Serial.println(moisture);
+  if (moisture > low && moisture < high){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
+  }
+  delay(1000);
+}
